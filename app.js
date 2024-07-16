d54f5ed0684bd2a5a14e4f77289fb0835c9113f8
Argel Hernandez <argel6767@gmail.com>
2024-07-16 00:41:09 -0400

    adds additonal weather info sections

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
         //checks if city is already in list
         if (!containsCity(cityInput)) {
         //if so fetches data
-        const API_URL = `http://api.weatherapi.com/v1/current.json?key=${weatherAPI}&q=${cityInput}`;
+        const API_URL = `http://api.weatherapi.com/v1/forecast.json?key=${weatherAPI}&q=${cityInput}`;
         const weatherData = await fetchWeatherData(API_URL);
         //creates li element
         addWeatherData(weatherData, cityInput);
@@ -48,6 +48,7 @@ const fetchWeatherData = async (url) => {
     throw new Error("Fetch request failed " + response.statusText);
   }
   const data = await response.json();
+  console.log(data)
   return data;
   }
   //if non valid city is given
@@ -61,18 +62,34 @@ const fetchWeatherData = async (url) => {
 const addWeatherData = async (weatherData, city) => {
   const citiesList = document.querySelector(".ajax-section .cities");
   const newCityWeather = document.createElement("li");
-  feelsLike = await weatherData.current.feelslike_f;
-  correctIcon = await correctTime(weatherData);
+  const weatherObject = await weatherData;
+  const currTemp = weatherObject.current.temp_f;
+  feelsLike = weatherObject.current.feelslike_f;
+  correctIcon = correctTime(weatherObject);
+  const humidity = weatherObject.current.humidity;
+  const windSpeed = weatherObject.current.wind_mph;
+  const rainChance = weatherObject.forecast.forecastday[0].day.daily_chance_of_rain;
   const weatherIconHTML = `<div class="weather-container">
-  <p>${capitalizeFirstLetter(city)} feels like: ${feelsLike}</p>
-  ${correctIcon}</div>`
+  <h2 id="city-name">${capitalizeFirstLetter(city)}</h2>
+  ${correctIcon}
+  <h3>${currTemp}&deg; F</h3>
+  <p id="feels-like">feels like: ${feelsLike}</p>
+  <div id="bottom-section">
+    <div class="bottom-section-third"><p>Humidity</p>
+    <p>${humidity}&deg;</p></div>
+    <div class="bottom-section-third"><p>Wind</p>
+    <p>${windSpeed}</p></div>
+    <div class="bottom-section-third"><p>Chance of rain</p>
+    <p>${rainChance}</p></div>
+  </div>
+  </div>`
   newCityWeather.innerHTML = weatherIconHTML;
   citiesList.appendChild(newCityWeather);
 }
 
 //puts correct image for weather 
-const correctTime = async (weatherData) => {
-  const cityTime = await weatherData.location.localtime;
+const correctTime = (weatherData) => {
+  const cityTime = weatherData.location.localtime;
   const actualTime = cityTime.split(" ")[1];
   const hour = parseInt(actualTime.split(":")[0],10);
 
